5e3ab8bed818432cb18c094de459c023dc0f334f
hai666l <0xconr@protonmail.com>
2020-07-21 19:50:14 -0500

    Created Project

---
+++ objectiveC/LSIAgent.h
@@ -0,0 +1,17 @@
+//
+//  LSIAgent.h
+//  objectiveC
+//
+//  Created by conner on 7/21/20.
+//  Copyright © 2020 conner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIAgent : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ objectiveC/LSIAgent.m
@@ -0,0 +1,13 @@
+//
+//  LSIAgent.m
+//  objectiveC
+//
+//  Created by conner on 7/21/20.
+//  Copyright © 2020 conner. All rights reserved.
+//
+
+#import "LSIAgent.h"
+
+@implementation LSIAgent
+
+@end
