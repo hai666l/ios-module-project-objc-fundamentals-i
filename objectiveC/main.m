50477f4b6c3a2aff8795be56f6c7d91c4eabdda7
hai666l <0xconr@protonmail.com>
2020-07-21 19:45:00 -0500

    Initial Commit

---
+++ objectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objectiveC
+//
+//  Created by conner on 7/21/20.
+//  Copyright © 2020 conner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
